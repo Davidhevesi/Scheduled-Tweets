51463aa36f3d2b17916a7413e42c9a2af47ef356
David Hevesi <davidhevesi2008@gmail.com>
2021-08-29 05:45:37 -0400

    create user table and sign up form

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence:true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get"about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 end
