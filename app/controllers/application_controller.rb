1ec2a09fd03046bfc8687b89ee70eb2d88a115ba
David Hevesi <davidhevesi2008@gmail.com>
2021-08-29 09:05:11 -0400

    Add option to change password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,7 @@ class ApplicationController < ActionController::Base
             Current.user = User.find_by(id: session[:user_id])
         end 
     end
+    def require_user_logged_in!
+      redirect_to sign_in_path, alert: "You must be sign in to do that" if Current.user.nil?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get"about", to: "about#index"
+  
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
