16ef01a1453730826714f7e0e1832f7c0f53a731
David Hevesi <davidhevesi2008@gmail.com>
2021-09-16 11:22:35 -0400

    Changes in the schema tabel

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_15_160807) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "twitter_account_id", null: false
