fc745050ab419a1e388276524239b2c80a42f1c0
David Hevesi <davidhevesi2008@gmail.com>
2021-09-16 10:57:45 -0400

    removed dev credeentials

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,6 +1,7 @@
 class TwitterAccount < ApplicationRecord
-  belongs_to :user
   has_many :tweets, dependenet: :destroy
+  belongs_to :user
+
   validates :username, uniqueness: true
 
   def client = Twitter::REST::Client.new do |config|
